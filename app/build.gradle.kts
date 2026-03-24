d46d5a34db93674576c9ae75e58c41d8ba2dcf7d
Bing Ho <bingho1@gmail.com>
2026-03-24 15:25:47 -0500

    Pin GeckoView version and enable resource shrinking
    - Pin GeckoView from "+" to "148.0.20260309125808" for reproducible
      builds
    - Enable isShrinkResources for release builds to reduce APK size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -116,6 +116,7 @@ configure<ApplicationExtension> {
     buildTypes {
         release {
             isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
