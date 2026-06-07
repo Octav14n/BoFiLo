0ecc61182054835ac3c7b94b4f190596028cd42c
Simon K <Oct4v14n@googlemail.com>
2026-06-07 19:03:15 +0200

    First try to be able to download images from AO3 (#143)
    * Fix apsw dependency of FanFicFare
    
    apsw is not compatible with Chaquopy
    
    * First try to be able to download images from AO3

---
+++ app/src/main/python/apsw/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="apsw",
+    version="0.0.0",
+    description="Stub package for apsw (Android dummy)",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+)
